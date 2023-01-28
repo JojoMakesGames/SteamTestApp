7b6b11ff411cb4752992674ae79d6cc04fe3103a
Jojo Oatey <jojo.oatey@gmail.com>
2023-01-28 13:36:37 -0500

    remove secret

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SteamTestApp
 Test app using steam integration
+
+Run docker command:
+docker run \
+    --publish=7474:7474 --publish=7687:7687 \
+    --volume=$HOME/neo4j/data:/data \
+    --env=NEO4J_AUTH=none neo4j
+
+set 
+run python ./api/database/create_graph.py
--- api/settings.py
+++ api/settings.py
@@ -1,9 +1,12 @@
+import configparser
 from fastapi import FastAPI
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    steam_key: str = "55F10D9E9AC8E75027FDE67170A39A78"
+    config = configparser.ConfigParser()  
+    config.read('api/local.config.ini')
+    steam_key: str = config.get('API', 'STEAM_API_TOKEN')
     steam_public_api: str = "http://store.steampowered.com/api"
     steam_private_api: str = "https://api.steampowered.com"
     my_user: int = 76561198139366501
