bbdb9f2a785a07e467d47eabdae27515a42d0089
Jojo Oatey <jojo.oatey@gmail.com>
2023-01-28 13:25:27 -0500

    intermediary commit while I break things

--- api/settings.py
+++ api/settings.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     steam_key: str = "55F10D9E9AC8E75027FDE67170A39A78"
     steam_public_api: str = "http://store.steampowered.com/api"
     steam_private_api: str = "https://api.steampowered.com"
-    my_user: int = 76561198045509954
+    my_user: int = 76561198139366501
 
 
 settings = Settings()
